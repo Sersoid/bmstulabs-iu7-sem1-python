aeb60e5b8544432b4b1c08d6f722a39282c62ec9
Sers <serspgl@gmail.com>
2021-10-06 10:55:31 +0300

    Added serifs for graph in lab4 Added new symbols for table and graph

--- 4/4.py
+++ 4/4.py
@@ -17,13 +17,14 @@
 start = float(input("Введите начальное значение: "))  # -0.8
 finish = float(input("Введите конечное значение: "))  # 1.2
 step = float(input("Введите длину шага: "))  # 0.05
+serifs_count = int(input("Введите кол-во засечек на оси графика: "))  # 4-8
 
 # Таблица
 print(
-    "y = b^9 + 34b^8 - 2b^7 + 24b^6 - 76b^5 + 33b^4 - b^3 + 3b^2 + 7b - 33\n"
-    "+-------------+-------------+\n"
-    "|      x      |      y      |\n"
-    "+-------------+-------------+"
+    "\ny = b^9 + 34b^8 - 2b^7 + 24b^6 - 76b^5 + 33b^4 - b^3 + 3b^2 + 7b - 33\n"
+    "┌─────────────┬─────────────┐\n"
+    "│      x      │      y      │\n"
+    "├─────────────┼─────────────┤"
 )
 
 steps_count = int((finish - start) / step)
@@ -45,9 +46,9 @@ for step_index in range(steps_count + 1):
     if y > max_y:
         max_y = y
 
-    print(f"| {'{:>11.5g}'.format(x)} | {'{:>11.5g}'.format(y)} |")
+    print(f"│ {'{:>11.5g}'.format(x)} │ {'{:>11.5g}'.format(y)} │")
 
-print("+-------------+-------------+")
+print("└─────────────┴─────────────┘")
 
 # График
 if min_y > 0 or max_y < 0:
@@ -55,9 +56,15 @@ if min_y > 0 or max_y < 0:
 else:
     x_axis = int(100 * (-min_y) / (max_y - min_y))
 
+serifs = "\n       "
+serif_step = 100 // (serifs_count - 1)
+value_step = (max_y - min_y) / (serifs_count - 1)
+for i in range(serifs_count):
+    serifs += ("{:<" + str(serif_step) + ".5g}").format(min_y + i * value_step)
+
 print(
-    f"\n         {'{:<50.5g}'.format(min_y)}{'{:>50.5g}'.format(max_y)}\n"
-    "   x     \\" + "-" * 98 + "/"
+    serifs + \
+    "\n   x     \\" + "─" * 98 + "/"
 )
 
 for step_index in range(steps_count + 1):
@@ -69,15 +76,15 @@ for step_index in range(steps_count + 1):
     if x_axis:
         if x_axis > point:
             if point == 0:
-                print(f"*{' ' * (x_axis - 2)}|")
+                print(f"○{' ' * (x_axis - 2)}│")
             else:
-                print(f"{' ' * (point - 1)}*{' ' * (x_axis - point - 1)}|")
+                print(f"{' ' * (point - 1)}○{' ' * (x_axis - point - 1)}│")
         elif x_axis < point:
-            print(f"{' ' * (x_axis - 1)}|{' ' * (point - x_axis - 1)}*")
+            print(f"{' ' * (x_axis - 1)}│{' ' * (point - x_axis - 1)}○")
         else:
-            print(f"{' ' * (x_axis - 1)}*")
+            print(f"{' ' * (x_axis - 1)}○")
     else:
-        print(f"{' ' * (point - 1)}*")
+        print(f"{' ' * (point - 1)}○")
 
 # Ответ на дополнительный вопрос
 print(f"\nМинимальное значение y = {format(min_y, '.5g')} достигается при x = {format(x_min_y, '.5g')}")
