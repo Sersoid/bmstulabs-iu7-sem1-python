d5c169907d24ac901560fb5c7f9c132bd5e95db2
Sers <serspgl@gmail.com>
2021-10-06 11:17:00 +0300

    Updated graph in lab4

--- 4/4.py
+++ 4/4.py
@@ -59,14 +59,14 @@ if min_y > 0 or max_y < 0:
 else:
     x_axis = int(100 * (-min_y) / (max_y - min_y))
 
-serifs = "\n       "
+serifs = "\n        "
 serif_step = 100 // (serifs_count - 1)
 value_step = (max_y - min_y) / (serifs_count - 1)
 for i in range(serifs_count):
     serifs += ("{:<" + str(serif_step) + ".5g}").format(min_y + i * value_step)
 
 print(
-    f"{serifs}\n   x     \\" + "─" * 98 + "/"
+    f"{serifs}\n   x   ┌──\\" + "─" * 98 + "/"
 )
 
 for step_index in range(steps_count + 1):
@@ -74,7 +74,7 @@ for step_index in range(steps_count + 1):
     y = x ** 9 + 34 * x ** 8 - 2 * x ** 7 + 24 * x ** 6 - 76 * x ** 5 + 33 * x ** 4 - x ** 3 + 3 * x ** 2 + 7 * x - 33
 
     point = int(100 * (y - min_y) / (max_y - min_y))
-    print("{:>6.5g}".format(x), end='    ')
+    print("{:>6.5g}".format(x), end=' │  ')
     if x_axis:
         if x_axis > point:
             if point == 0:
@@ -87,6 +87,8 @@ for step_index in range(steps_count + 1):
             print(f"{' ' * (x_axis - 1)}○")
     else:
         print(f"{' ' * (point - 1)}○")
+
+print("─" * 7 + "┴──/" + "─" * 98 + "\\")
 
 # Ответ на дополнительный вопрос
 print(f"\nМинимальное значение y = {format(min_y, '.5g')} достигается при x = {format(x_min_y, '.5g')}")
