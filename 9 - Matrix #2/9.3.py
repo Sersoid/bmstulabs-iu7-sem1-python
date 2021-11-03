aefd238225612171f5a99059e11eee84eb3e1e67
Sers <serspgl@gmail.com>
2021-11-03 13:52:21 +0300

    Added 3 task of lab9

---
+++ 9 - Matrix #2/9.3.py
@@ -0,0 +1,28 @@
+# Ввод данных
+n = int(input("Введите размер квадратной матрицы: "))
+
+A = []
+
+# Основной блок программы
+for i in range(n):
+    row = list(map(float, input(f"Введите {i + 1} строку матрицы: ").split()))
+
+    row_length = len(row)
+    if row_length < n:
+        for _ in range(n - row_length):
+            row.append(0)
+    elif row_length > n:
+        row = row[0:n]
+
+    A.append(row)
+
+for i in range(n):
+    for j in range(n):
+        if i < j:
+            A[i][j], A[j][i] = A[j][i], A[i][j]
+
+# Вывод
+print("\nМатрица:")
+
+for matrix_row in A:
+    print(*[format(i, ">9.5g") for i in matrix_row])
