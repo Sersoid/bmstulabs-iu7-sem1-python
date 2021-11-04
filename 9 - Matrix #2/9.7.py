3e2103cddbdec6527ad077827145c31076e0c81d
Sers <serspgl@gmail.com>
2021-11-04 14:48:20 +0300

    Added 7 task of lab9

---
+++ 9 - Matrix #2/9.7.py
@@ -0,0 +1,29 @@
+# Ввод
+n = int(input("Введите кол-во строк матрицы: "))
+m = int(input("Введите кол-во столбцов матрицы: "))
+
+A = []
+
+# Основной блок программы
+for i in range(n):
+    row = input(f"Введите {i + 1} строку матрицы: ").split()
+
+    row_length = len(row)
+
+    if row_length < n:
+        for _ in range(n - row_length):
+            row.append(" ")
+    elif row_length > n:
+        row = row[0:n]
+
+    for j in range(m):
+        row[j] = row[j][0]
+        if row[j] in ("a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"):
+            row[j] = "."
+
+    A.append(row)
+
+# Вывод
+print("\nМатрица D:")
+for matrix_row in A:
+    print(*[f"'{i}'" for i in matrix_row])
